dda5461d270da03ca4d16554350154e033aeb691
JOSE.BELLOZ <JOSE.BELLOZ@OFITE-GRUDE2>
2022-12-01 13:55:06 -0500

    Agregar archivos de proyecto.

---
+++ EnviosTLE/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnviosTLE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RegistroEnvio()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+       
+        
+    }
+}
---
+++ EnviosTLE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EnviosTLE.MvcApplication" Language="C#" %>
